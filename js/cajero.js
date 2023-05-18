852ff1ed5b4d7ef03d58a3815897c3ef9c123d74
ANA TORRES <anatorreshtp@gmail.com>
2023-05-18 03:18:45 -0500

    montos

--- js/cajero.js
+++ js/cajero.js
@@ -7,4 +7,26 @@ let montos ={
 let url = new URL(window.location.href);
 console.log(url);
 let numCta = url.searchParams.get('numCta');
-console.log(numCta);
+
+function actualizarSaldo(){
+    const saldoActual =document.getElementById('saldoActual');
+    saldoActual.innerHTML ='$' + montos[numCta];
+
+}
+function actualizarNumUsuario(){
+    const numUsuario =document.getElementById('numUsuario');
+    numUsuario.innerHTML = numCta;
+
+}
+actualizarSaldo();
+actualizarNumUsuario();
+
+const inputMonto = document.getElementById('inputMonto');
+const ingresarMonto= document.getElementById('ingresarMonto');
+ingresarMonto.addEventListener('click', function(e){
+    let monto = inputMonto.value;
+    monto= parseInt(monto);
+    montos[numCta]= montos[numCta]+monto;
+    actualizarSaldo();
+    inputMonto.value='';
+});
